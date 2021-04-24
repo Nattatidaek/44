f2f25c8a62fd5e3ef9f437c523a1b910ad0c9487
MINWINPC\UserPC <Nattatida.ek1111@mail.kmutt.ac.th>
2021-04-24 15:25:43 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x,y,dx,dy step;
+            Console.Write("Input X1:");
+            float x1 = float.Parse(Console.ReadLine());
+            Console.Write("Input X2:");
+            float x2 = float.Parse(Console.ReadLine());
+            Console.Write("Input Y1:");
+            float y1 = float.Parse(Console.ReadLine());
+            Console.Write("Input Y2:");
+            float Y2 = float.Parse(Console.ReadLine());
+
+            dx = x2 - x1;
+            dy = Y2 - y1;
+        }
+}
